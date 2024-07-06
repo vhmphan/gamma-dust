c2984a53d6d14f2594726c7c9aae8d9aa0ad7b45
Vo Hong Minh Phan <82878997+vhmphan@users.noreply.github.com>
2024-07-06 16:27:38 +0200

    Add notes for 1_CR_map.py

--- 1_CR_map.py
+++ 1_CR_map.py
@@ -65,18 +65,18 @@ pCR.plot_emissivity_LOC(qg_Geant4,Eg,rg,zg)
 hdul=fits.open('samples_densities_hpixr.fits')
 print(hdul.info())
 
-rs=(hdul[2].data)['radial pixel edges'].astype(np.float64) # Edges of radial bins
-drs=np.diff(rs)*3.086e21
-rs=(hdul[1].data)['radial pixel centres'].astype(np.float64)*1.0e3 # Edges of radial bins
+rs=(hdul[2].data)['radial pixel edges'].astype(np.float64) # kpc -> Edges of radial bins
+drs=np.diff(rs)*3.086e21 # cm -> Radial bin width for line-of-sight integration
+rs=(hdul[1].data)['radial pixel centres'].astype(np.float64)*1.0e3 # pc -> Centres of radial bins for interpolating cosmic-ray distribution
 samples_HI=(hdul[3].data).T # cm^-3
 samples_H2=(hdul[4].data).T # cm^-3
 hdul.close()
 ngas=2.0*samples_H2+samples_HI # cm^-3
 
-# Interpolate cosmic-ray distribution on healpix-r grid as gas
+# Interpolate gamma-ray emissivity on healpix-r grid as gas
 N_sample, N_rs, N_pix=samples_HI.shape
 NSIDE=int(np.sqrt(N_pix/12))
-qg_Geant4_healpixr=pCR.get_healpix_interp(qg_Geant4,Eg,rg,zg,rs,NSIDE) # GeV^-1 s^-1
+qg_Geant4_healpixr=pCR.get_healpix_interp(qg_Geant4,Eg,rg,zg,rs,NSIDE) # GeV^-1 s^-1 -> Interpolate gamma-ray emissivity
 
 # Compute the diffuse emission in all gas samples
 gamma_map=np.sum(ngas[:,np.newaxis,:,:]*qg_Geant4_healpixr[np.newaxis,:,:,:]*drs[np.newaxis,np.newaxis,:,np.newaxis],axis=2) # GeV^-1 cm^-2 s^-1
@@ -84,7 +84,7 @@ gamma_map=np.sum(ngas[:,np.newaxis,:,:]*qg_Geant4_healpixr[np.newaxis,:,:,:]*drs
 # Record the time finishing computing cosmic-ray map
 end_time=time.time()
 
-# Calculate the elapsed time
+# Calculate the computing time
 elapsed_time_CR=CR_time-start_time
 elapsed_time_gamma=end_time-CR_time
 
