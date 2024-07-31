8019daedf3ad642f9ccebbc6bf936d373de24ad0
vhmphan <vhmphan@gmail.com>
2024-07-31 10:35:58 +0200

    Improve neural network

--- NN/0_nn.py
+++ NN/0_nn.py
@@ -68,22 +68,13 @@ ngas_mean = jnp.mean(ngas, axis=0)[jnp.newaxis, :, :]
 
 # Define the properties of the neural network
 N_SAMPLES = 51
-LAYERS = [1, 5, 5, 5, 1]
+LAYERS = [1, 10, 10, 10, 1]
 LEARNING_RATE = 0.1
-N_EPOCHS = 200
+N_EPOCHS = 20000
 epoch_print = N_EPOCHS / 10
 
+# Random key
 key = jax.random.PRNGKey(42)
-key, ynoisekey = jax.random.split(key, 2)
-
-# Spatial domain for the function gSNR to be inferred and renormalize for the neural network
-x_samples_raw = jnp.linspace(0, 15.0, N_SAMPLES)[:, jnp.newaxis]
-x_samples = x_samples_raw / x_samples_raw[-1]
-
-# Mock data for testing
-y_sol = jCR.func_gSNR_YUK04(jnp.array([8178.0])) / jnp.exp(0.4)
-y_grtruth = jnp.log((jCR.func_gSNR_YUK04(x_samples_raw * 1.0e3)) / (y_sol))
-y_samples = y_grtruth
 
 # Weight initialization
 weight_matrices = []
@@ -117,13 +108,33 @@ def network_forward(x, weights, biases, activations):
         a = f(a @ W + b)
     return a
 
+# Random key for mock data
+key, ynoisekey = jax.random.split(key, 2)
+
+# Spatial domain for the function gSNR to be inferred and renormalize for the neural network
+x_samples_raw = jnp.linspace(0, 15.0, N_SAMPLES)[:, jnp.newaxis]
+x_samples = x_samples_raw / x_samples_raw[-1]
+
+# Mock data for testing
+y_init = jnp.interp(8.178,
+                        x_samples_raw.ravel(),
+                        network_forward(x_samples_raw*1.0e3,weight_matrices,bias_vectors,activation_functions).ravel())
+
+y_sol = 2.0e-9 # jCR.func_gSNR_YUK04(jnp.array([8178.0])) / jnp.exp(y_init)
+y_grtruth = jnp.log((jCR.func_gSNR_YUK04(x_samples_raw * 1.0e3) +5.0e-9*jnp.exp(-(x_samples_raw-9.0)**2/2.0)) / (y_sol))
+y_samples_raw = y_grtruth
+y_samples = jnp.log(jCR.func_gamma_map_gSNR(((x_samples_raw.ravel()) * 1.0e3, (jnp.exp(y_samples_raw) * y_sol).ravel()), pars_prop, zeta_n, dXSdEg_Geant4, ngas_mean, drs, points_intr, E))[0, 0, mask]
+
 # Loss function to compare gamma-ray maps
 def loss_forward(y_guess, y_ref):
     gamma_guess = jnp.log(jCR.func_gamma_map_gSNR(((x_samples_raw.ravel()) * 1.0e3, (jnp.exp(y_guess) * y_sol).ravel()), pars_prop, zeta_n, dXSdEg_Geant4, ngas_mean, drs, points_intr, E))
-    gamma_ref = jnp.log(jCR.func_gamma_map_gSNR(((x_samples_raw.ravel()) * 1.0e3, (jnp.exp(y_ref) * y_sol).ravel()), pars_prop, zeta_n, dXSdEg_Geant4, ngas_mean, drs, points_intr, E))
-    delta = (gamma_guess[0, 0, mask] - gamma_ref[0, 0, mask]) / (jnp.mean(gamma_ref[0, 0, mask]))
-    return 0.5 * jnp.mean(delta**2)
+    # gamma_ref = jnp.log(jCR.func_gamma_map_gSNR(((x_samples_raw.ravel()) * 1.0e3, (jnp.exp(y_ref) * y_sol).ravel()), pars_prop, zeta_n, dXSdEg_Geant4, ngas_mean, drs, points_intr, E))
+    delta = gamma_guess[0, 0, mask] - y_ref
+    # delta = y_guess - y_ref
 
+    return 100.0 * jnp.mean(delta**2)
+
+# Function to find derivatives with respect to weight matrices and bias vectors
 loss_and_grad_fun = jax.value_and_grad(
     lambda Ws, bs: loss_forward(
         network_forward(
@@ -162,7 +173,7 @@ for epoch in range(N_EPOCHS):
     if (epoch + 1) % epoch_print == 0:
         print(f"epoch: {epoch+1}, loss: {loss}")
         weights, biases = params
-        plt.scatter(x_samples_raw, jnp.exp(y_samples) * y_sol, label='Samples')
+        plt.scatter(x_samples_raw, jnp.exp(y_samples_raw) * y_sol, label='Samples')
         plt.scatter(x_samples_raw, jnp.exp(network_forward(x_samples, weights, biases, activation_functions)) * y_sol, label='Interation %d' % (epoch + 1))
         plt.plot(x_samples_raw, jnp.exp(y_grtruth) * y_sol, 'k--', label='Ground truth')
         plt.legend(loc='upper left')
@@ -193,6 +204,5 @@ weights_dict = {f"weight_{i}": np.array(weights[i]) for i in range(len(weights))
 biases_dict = {f"bias_{i}": np.array(biases[i]) for i in range(len(biases))}
 save_dict = {**weights_dict, **biases_dict}
 np.savez('nn.npz', **save_dict)
-
 
 print('Run time: %.2f seconds' % (time.time() - start_time))
